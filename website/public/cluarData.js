debd0b87c23a5a88f54f9e75a44ae86bbffd298f
Luismijias <luismijias@gmail.com>
2023-02-21 22:28:04 -0300

    Upando arquivos ignorados pelo .gitignore do website e ui.

---
+++ website/public/cluarData.js
@@ -0,0 +1,170 @@
+window.cluarData = {
+    "dictionary": {
+        "PT": {
+            "contact-form-success": "<p>Os dados foram enviados com sucesso, obrigado.<\/p>",
+            "contact-form-send": "<p>Enviar<\/p>",
+            "contact-form-name": "<p>Nome<\/p>",
+            "contact-form-message": "<p>Mensagem<\/p>",
+            "contact-form-validate-message-required": "<p>${label} \u00E9 de preenchimento obrigat\u00F3rio.<br><\/p>",
+            "contact-form-validate-message-email": "<p>${label} n\u00E3o \u00E9 um e-mail v\u00E1lido.<br><\/p>",
+            "contact-form-fail": "<p>N\u00E3o foi poss\u00EDvel enviar os dados, tente novamente mais tarde.<\/p>",
+            "contact-form-email": "<p>E-mail<\/p>",
+            "contact-form-subject": "<p>T\u00EDtulo<\/p>"
+        },
+        "EN": {
+            "contact-form-success": "<p>Data has been sent successfully, thank you.<br><\/p>",
+            "contact-form-send": "<p>Send<\/p>",
+            "contact-form-name": "<p>Name<\/p>",
+            "contact-form-message": "<p>Message<\/p>",
+            "contact-form-validate-message-required": "<p>${label} is mandatory.<br><\/p>",
+            "contact-form-validate-message-email": "<p>${label} is not a valid e-mail.<br><\/p>",
+            "contact-form-fail": "<p>Unable to send data, please try again later.<br><\/p>",
+            "contact-form-email": "<p>E-mail<\/p>",
+            "contact-form-subject": "<p>Subject<\/p>"
+        }
+    },
+    "pages": {
+        "PT": [{
+            "parent": "",
+            "uid": "0194d0aa-c5ec-4f9d-abab-de6298c5f9e9",
+            "keywords": "",
+            "sorter": 10,
+            "navigable": true,
+            "menu_title": "",
+            "link": "/",
+            "description": "",
+            "menu": true,
+            "title": "Principal",
+            "structure": [
+                {
+                    "image": "default-banner-background-SBiogyY7.jpg",
+                    "uid": "5126f3bb-51d1-489e-89dc-08087ed38e5e",
+                    "sorter": 10,
+                    "image_alt": "",
+                    "section": "banner",
+                    "image_title": "",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "title": "Prestes a Voar",
+                    "type": "default",
+                    "actions": [],
+                    "content": "<p>Aqui \u00E9 onde voc\u00EA lan\u00E7a websites em uma outra dimens\u00E3o.<\/p>"
+                },
+                {
+                    "image": "",
+                    "uid": "f110a9d1-c9cd-40a3-9359-7b472234e0ac",
+                    "image_max_width": "0",
+                    "sorter": 20,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Documenta\u00E7\u00E3o",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p>Veja a documenta\u00E7\u00E3o oficial online em:<\/p><ul><li><a href=\"https://github.com/netuno-org/cluar/blob/main/docs/README-pt_PT.md\" target=\"_blank\">Documenta\u00E7\u00E3o Oficial no GitHub<\/a><\/li><\/ul><p>A vers\u00E3o offline est\u00E1 na pasta <b>docs<\/b> que est\u00E1 na ra\u00EDz deste projeto, a\u00ED \u00E9 onde vai encontrar o README inicial.<\/p>"
+                },
+                {
+                    "image": "",
+                    "uid": "ed312fe0-b839-4367-82c1-1445464b39d0",
+                    "image_max_width": "0",
+                    "sorter": 30,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "C\u00F3digo Aberto & Comunidade",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p>Todas as tecnologias utilizadas aqui s\u00E3o livres e abertas.<\/p><p>Colabore sugerindo melhorias ou reportando problemas em&nbsp;<a href=\"https://github.com/netuno-org/cluar/issues\" target=\"_blank\">issues<\/a>.<\/p><p>Para ajudar a comunidade publique as tuas d\u00FAvidas e dificuldades no <a href=\"https://forum.netuno.org/\" target=\"_blank\">forum<\/a>.&nbsp;<br><\/p><p>Visite o site do <a href=\"https://www.netuno.org/\" target=\"_blank\">Netuno<\/a>&nbsp;e siga-nos nas redes sociais e participe do nosso servidor no discord, os links est\u00E3o no rodap\u00E9.<\/p>"
+                }
+            ]
+        }],
+        "EN": [{
+            "parent": "",
+            "uid": "5002a742-e092-4c0b-8536-546bd1319c7f",
+            "keywords": "",
+            "sorter": 10,
+            "navigable": true,
+            "menu_title": "",
+            "link": "/",
+            "description": "",
+            "menu": true,
+            "title": "Home",
+            "structure": [
+                {
+                    "image": "default-banner-background-QBEnNGtC.jpg",
+                    "uid": "9302bc90-d687-494d-bfc0-b4aeff4df09e",
+                    "sorter": 10,
+                    "image_alt": "",
+                    "section": "banner",
+                    "image_title": "",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "title": "Ready to Fly",
+                    "type": "default",
+                    "actions": [],
+                    "content": "<p>Here is where you launch websites into another dimension.<\/p>"
+                },
+                {
+                    "image": "",
+                    "uid": "b0697f9e-3eb0-468f-bb71-13d5a9e211fe",
+                    "image_max_width": "0",
+                    "sorter": 20,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Documentation",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p>See the official documentation online at:<\/p><ul><li><a href=\"https://github.com/netuno-org/cluar/blob/main/docs/README.md\" target=\"_blank\">Official Documentation on GitHub<\/a><\/li><\/ul><p>The offline version is in the <b>docs<\/b> folder at the root of this project, that's where you'll find the initial README.<\/p>"
+                },
+                {
+                    "image": "",
+                    "uid": "d7564744-f70f-4859-9070-86a3b2a83e91",
+                    "image_max_width": "0",
+                    "sorter": 30,
+                    "image_alt": "",
+                    "section": "content",
+                    "image_title": "",
+                    "title": "Open Source & Community",
+                    "type": "text",
+                    "actions": [],
+                    "content": "<p>All technologies used here are free and open.<\/p><p>Collaborate by suggesting improvements or reporting problems in <a href=\"https://github.com/netuno-org/cluar/issues\" target=\"_blank\">issues<\/a>.<\/p><p>To help the community post your doubts and difficulties in the <a href=\"https://forum.netuno.org/\" target=\"_blank\">forum<\/a>.&nbsp;<br><\/p><p>Visit <a href=\"https://www.netuno.org/\" target=\"_blank\">Netuno<\/a>'s website and follow us on social media and join our discord server, the links are in the footer.<\/span><\/p>"
+                }
+            ]
+        }]
+    },
+    "languages": [
+        {
+            "default": true,
+            "code": "PT",
+            "description": "Portugu\u00EAs",
+            "locale": "pt"
+        },
+        {
+            "default": false,
+            "code": "EN",
+            "description": "English",
+            "locale": "en"
+        }
+    ],
+    "configuration": {
+        "GENERIC": {
+            "footer-phone": "99999 9999",
+            "map-latitude": "38.7038342",
+            "footer-address": "Netuno.org, Cluar CMS\r\nOpen Source",
+            "map-longitude": "-8.9698548"
+        },
+        "PT": {"footer-email": "meu@e-mail.org"},
+        "EN": {"footer-email": "my@e-mail.org"}
+    },
+    "config": {
+        "mapbox": {"dark": false},
+        "name": "Website Name",
+        "services": {"api": "http://localhost:9000/services/"},
+        "url": "http://localhost:3000"
+    }
+};
